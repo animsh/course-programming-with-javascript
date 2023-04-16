b81eaa7aefb92a774998de77d336e4635d628e5a
Sagar More <animsh.more@gmail.com>
2023-04-16 23:16:49 +0530

    week 1

---
+++ week 1/06_repetetive_tasks_with_loops.js
@@ -0,0 +1,30 @@
+for (let i = 1; i < 6; i++) {
+  console.log(i);
+}
+
+console.log("Counting completed!");
+
+for (let i = 5; i > 0; i--) {
+  console.log(i);
+}
+console.log("Countdown finished!");
+
+let i = 1;
+while (i < 6) {
+  console.log(i);
+  i++;
+}
+console.log("Counting completed!");
+
+i = 5;
+while (i > 0) {
+  console.log(i);
+  i--;
+}
+console.log("Countdown finished!");
+
+i = 2018;
+while (i < 2023) {
+  console.log(i);
+  i++;
+}
